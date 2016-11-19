2a0168d96ddde032e5ee0e29f34bd630395ef938
scottnv99 <scottnv99@yahoo.com>
2016-11-19 13:13:02 -0800

    Added randomization for latency output

--- src/DeVogeLitzMod/networkLatencyGenerator.java
+++ src/DeVogeLitzMod/networkLatencyGenerator.java
@@ -19,6 +19,7 @@ public class networkLatencyGenerator extends ViewableAtomic{
 	
 	protected double int_arr_time;
 	protected String network_latency;
+	protected int latency; 
                                     
 	public networkLatencyGenerator() {
 		this("networkLatencyGenerator", 30);
@@ -30,6 +31,7 @@ public class networkLatencyGenerator extends ViewableAtomic{
 	   addOutport("out");
 	   int_arr_time = Int_arr_time ;	
 	   network_latency = "none";
+	   latency = 1;	
 	}
 	    
 	public void initialize() {
@@ -42,6 +44,7 @@ public class networkLatencyGenerator extends ViewableAtomic{
 		if(phaseIs("passive")) {
 			for (int i=0; i< x.getLength();i++) {
 				if (messageOnPort(x,"in",i)) {
+					latency = (int) (Math.random()*3) + 1;
 					holdIn("active", int_arr_time);						
 				}
 			}
@@ -64,6 +67,15 @@ public class networkLatencyGenerator extends ViewableAtomic{
 	public message out( ) {
 		message m = new message();
 		if (phaseIs("active")) {
+			if (latency = 1){
+				network_latency = "none";
+			}
+			else if (latency = 2){
+				network_latency = "medium";
+			}
+			else{
+				network_latency ="high";
+			}
 			m.add(makeContent("out", new Pair(new entity("network latency"), new entity(network_latency))));
 		}
 		return m;
